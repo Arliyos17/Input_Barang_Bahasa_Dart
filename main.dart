cfd24030f5d07f8890cb6bdbc766d16b19d1e22d
Arliyos17 <yosdilif17@gmail.com>
2023-05-25 09:55:14 +0700

    v1.0 main.dart

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:assesment_2_6701213007/inputbarang.dart';
+
+void main() => runApp(
+  MaterialApp(
+    title: 'Latihan Fluter',
+    home: BarangScreen(),
+  )
+);
